c1e461bf31a9ce9d2e6b68b4e98095f907f821d8
Natikana <mashkovski82@gmail.com>
2022-07-23 11:13:20 +0200

    Updates

---
+++ precache-manifest.9b7428187a541dc58fe52b7a3ebef792.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a77a1517273c3a6df85d770a1400bc14",
+    "url": "/Task/index.html"
+  },
+  {
+    "revision": "608518fbbb49cdf09651",
+    "url": "/Task/static/css/main.4ce639d4.chunk.css"
+  },
+  {
+    "revision": "92faccda99ac8a3f68f1",
+    "url": "/Task/static/js/2.6f3c9da1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Task/static/js/2.6f3c9da1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "608518fbbb49cdf09651",
+    "url": "/Task/static/js/main.c56c15e9.chunk.js"
+  },
+  {
+    "revision": "b76e0157253a298a7ebb",
+    "url": "/Task/static/js/runtime-main.24643982.js"
+  }
+]);
